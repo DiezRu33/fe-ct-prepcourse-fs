129cd7c0197447c8539d0ffc2ba58a1bf1b730da
diegoruffa@hotmail.com <diegoruffa@hotmail.com>
2024-02-26 23:02:12 -0300

    agregando script

--- CarpetaHenry/HTMLHomework/script.js
+++ CarpetaHenry/HTMLHomework/script.js
@@ -1,17 +1,7 @@
-function sortArray(arrayOfStrings) {
-  // Recibes un arreglo de strings.
-  // Debe retornar un nuevo arreglo, pero con las palabras ordenadas en orden creciente a partir
-  // de la longitud de cada string.
-  // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
-  // Tu código:
 
-  // retorna palabras ordenadas (menor a mayor: longitud)
-  let arregloString = [];
-  let cuentaCaracteres = 0;
-
-  arrayOfStrings.sort(( a, b) => b.length - a.length)
-
-  console.log(arrayOfStrings)
+function obj( obj, prop){
+  return prop in obj
 }
 
-sortArray((['You', 'are', 'beautiful', 'looking']))
+var resu = obj({ x: 1, y: 2}, 'x')
+console.log(resu)
